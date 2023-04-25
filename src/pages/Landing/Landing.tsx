8c2d1ecf8cbff936f640bf9ae1fe2802e5e175c0
huseyn2000 <huseyn.naghiyev@ufaz.az>
2023-04-25 15:08:27 +0400

    react_project

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,589 @@
+import { FirstPart } from "./FirstPart/FirstPart";
+import "./Landing.scss";
+
+export const Landing = () => {
+  return (
+    <>
+      <FirstPart />
+
+      <div className="second_page">
+        <div className="texts">
+          <div className="text_part1">
+            <div className="about">ABOUT US</div>
+
+            <h2>
+              The company leads entire <br />
+              webdesign process from <br />
+              concept to delivery.
+            </h2>
+          </div>
+
+          <div className="text_part2">
+            <h3>The Era Of Technology.</h3>
+
+            <p>
+              Through True Rich Attended does no end it his mother since real
+              had
+              <br />
+              half every him case in packages enquire we up ecstatic unsatiable
+              <br />
+              saw his giving Remain expense you position concluded.
+            </p>
+          </div>
+        </div>
+
+        <div className="container">
+          <img src="./second_page_assets/Image 1.png" alt="" />
+          <img src="./second_page_assets/Image 2.png" alt="" />
+          <img src="./second_page_assets/Image 3.png" alt="" />
+        </div>
+
+        <div className="second_page_footer">
+          <div className="first_part">
+            <div>
+              <h3>1560+</h3>
+              <img src="./second_page_assets/Group 1710.svg" alt="" />
+              <p>Project Delivered</p>
+            </div>
+
+            <div>
+              <h3>100+</h3>
+              <img src="./second_page_assets/Group 1710.svg" alt="" />
+              <p>Professional</p>
+            </div>
+
+            <div>
+              <h3>950+</h3>
+              <img src="./second_page_assets/Group 1710.svg" alt="" />
+              <p>Happy Client</p>
+            </div>
+
+            <div>
+              <h3>10 yrs</h3>
+              <img src="./second_page_assets/Group 1710.svg" alt="" />
+              <p>Experience</p>
+            </div>
+          </div>
+
+          <div className="sec_part">
+            <a href="#">Read about us</a>
+            <img src="./second_page_assets/footer_arrow.svg" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="third_page">
+        <div className="content">
+          <div className="left">
+            <div className="expertise">Our expertise</div>
+
+            <h2>
+              We want to get local identification in every <br />
+              corner of the world in this <br />
+              era of global citizenship
+            </h2>
+
+            <p>
+              Through True Rich Attended does no end it his mother since real
+              had half <br />
+              every him case in packages enquire we up ecstatic unsatiable saw
+              his giving <br />
+              Remain expense you position concluded.
+            </p>
+          </div>
+
+          <div className="right">
+            <div className="blue"></div>
+
+            <div className="frst_box">
+              <div>
+                <img src="./third_page/Group 1749.svg" alt="" />
+              </div>
+
+              <div className="text1">
+                <h6>On Time Delivery</h6>
+                <p>
+                  Through True Rich Attended does no end it <br />
+                  his mother since real had half every him.
+                </p>
+              </div>
+            </div>
+            <div className="scnd_box">
+              <div>
+                <img src="./third_page/Group 1755.svg" alt="" />
+              </div>
+
+              <div className="text1">
+                <h6>Best Quality</h6>
+                <p>
+                  Through True Rich Attended does no end it <br />
+                  his mother since real had half every him.
+                </p>
+              </div>
+            </div>
+            <div className="thrd_box">
+              <div>
+                <img src="./third_page/Group 1755.svg" alt="" />
+              </div>
+
+              <div className="text1">
+                <h6>Support Assist</h6>
+                <p>
+                  Through True Rich Attended does no end it <br />
+                  his mother since real had half every him.
+                </p>
+                <img src="./third_page/Group 1748.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="fourth_page">
+        <div className="content">
+          <div className="up_part">
+            <div className="small_txt">Our Services</div>
+
+            <h2>
+              We build software solutions <br />
+              that solve client's business <br />
+              challenges
+            </h2>
+
+            <button>
+              <img
+                id="shape4"
+                src="./first_pages_assets/Shapes.svg"
+                alt="shape"
+              />
+
+              <div>Start a Project</div>
+
+              <div>
+                <img src="./first_pages_assets/Icon.svg" alt="" />
+              </div>
+            </button>
+          </div>
+
+          <div className="down_part">
+            <div className="container">
+              <div className="first">
+                <img src="./fourth_page/Icon Div (2).svg" alt="" />
+
+                <h6>Technical support</h6>
+
+                <p>
+                  We aim to attain the greatest satisfaction for our clients and
+                  be one of the prominent names when it comes to world-className
+                  service
+                </p>
+
+                <a href="#">
+                  Read more <img src="./fourth_page/arrow.svg" alt="" />
+                </a>
+              </div>
+              <div className="second">
+                <img src="./fourth_page/Icon Div (2).svg" alt="" />
+
+                <h6>Testing Management</h6>
+
+                <p>
+                  We aim to attain the greatest satisfaction for our clients and
+                  be one of the prominent names when it comes to world-className
+                  service
+                </p>
+
+                <a href="#">
+                  Read more <img src="./fourth_page/arrow.svg" alt="" />
+                </a>
+              </div>
+              <div className="third">
+                <img src="./fourth_page/Icon Div (2).svg" alt="" />
+
+                <h6>Development</h6>
+
+                <p>
+                  We aim to attain the greatest <br />
+                  satisfaction for our clients and be one of the prominent names
+                  when it comes to world-className service
+                </p>
+
+                <a href="#">
+                  Read more <img src="./fourth_page/arrow.svg" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="fifth_page">
+        <div className="content">
+          <div className="up_part">
+            <div className="small_txt">Our Process</div>
+
+            <h2>
+              The process we are working <br />
+              With Our client Worldwide
+              <img
+                id="logo_right"
+                src="./fifth_page_assests/Shapes.svg"
+                alt=""
+              />
+            </h2>
+
+            <p>
+              Through True Rich Attended does no end it his mother since real
+              had half every <br />
+              him case in packages enquire we up ecstatic unsatiable.
+            </p>
+          </div>
+
+          <div className="down_part">
+            <div className="down">
+              <div className="f_f">
+                <div>
+                  <img src="./fifth_page_assests/Icon Div.svg" alt="" />
+                </div>
+
+                <div className="text1">
+                  <h6>Discover</h6>
+                  <p>
+                    We aim to attain the greatest <br />
+                    satisfaction for our clients and be <br />
+                    one of the prominent.
+                  </p>
+                </div>
+                <p className="numb">01</p>
+              </div>
+              <div className="f_f">
+                <div>
+                  <img src="./fifth_page_assests/Icon Div (1).svg" alt="" />
+                </div>
+
+                <div className="text1">
+                  <h6>Designing</h6>
+                  <p>
+                    We aim to attain the greatest <br />
+                    satisfaction for our clients and be <br />
+                    one of the prominent.
+                  </p>
+                </div>
+                <p className="numb">02</p>
+              </div>
+              <div className="f_f">
+                <div>
+                  <img src="./fifth_page_assests/Icon Div (2).svg" alt="" />
+                </div>
+
+                <div className="text1">
+                  <h6>Development</h6>
+                  <p>
+                    We aim to attain the greatest <br />
+                    satisfaction for our clients and be <br />
+                    one of the prominent.
+                  </p>
+                </div>
+                <p className="numb">03</p>
+              </div>
+            </div>
+
+            <div className="down">
+              <div className="f_f">
+                <div>
+                  <img src="./fifth_page_assests/Icon Div (3).svg" alt="" />
+                </div>
+
+                <div className="text1">
+                  <h6>Testing</h6>
+                  <p>
+                    We aim to attain the greatest <br />
+                    satisfaction for our clients and be <br />
+                    one of the prominent.
+                  </p>
+                </div>
+                <p className="numb">04</p>
+              </div>
+              <div className="f_f">
+                <div>
+                  <img src="./fifth_page_assests/Icon Div (4).svg" alt="" />
+                </div>
+
+                <div className="text1">
+                  <h6>Deployment</h6>
+                  <p>
+                    We aim to attain the greatest <br />
+                    satisfaction for our clients and be <br />
+                    one of the prominent.
+                  </p>
+                </div>
+                <p className="numb">05</p>
+              </div>
+              <div className="f_f">
+                <div>
+                  <img src="./fifth_page_assests/Icon Div (5).svg" alt="" />
+                </div>
+
+                <div className="text1">
+                  <h6>Maintenance</h6>
+                  <p>
+                    We aim to attain the greatest <br />
+                    satisfaction for our clients and be <br />
+                    one of the prominent.
+                  </p>
+                </div>
+                <p className="numb">06</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sixth_page">
+        <div className="content">
+          <div className="left">
+            <div className="blue"></div>
+
+            <h2>
+              Our customers love <br />
+              what we do
+            </h2>
+
+            <p>Transform your idea into reality with finsweet</p>
+
+            <p className="snd_p">
+              It is a long established fact that a reader will be distracted by
+              <br />
+              the readable content of a page when looking at its layout.
+            </p>
+
+            <div className="photo_cont">
+              <img src="./sixth_page/Image 3.png" alt="" />
+              <img src="./sixth_page/Image 2.png" alt="" />
+              <img src="./sixth_page/Image 1.png" alt="" />
+            </div>
+
+            <p className="last_p">30+ Customer Reviews</p>
+          </div>
+
+          <div className="right">
+            <div className="quote">
+              <img src="./sixth_page/Quote.svg" alt="" />
+            </div>
+            <div className="content">
+              <div className="h4_className">
+                <h4>
+                  Finsweet has been a wonderful partner <br />
+                  to work with. I have been a customer <br />
+                  now for the past few months now and I <br />
+                  have had nothing but positive experiences!
+                </h4>
+              </div>
+
+              <div className="footer">
+                <div className="row">
+                  <div>
+                    <img src="./sixth_page/Image.png" alt="" />
+                  </div>
+
+                  <div>
+                    <h6>Johnny Andro</h6>
+                    <p>Director, Company</p>
+                  </div>
+                </div>
+
+                <div className="logoplusim">
+                  <img src="./sixth_page/footer_logo.svg" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="circle">
+              <img src="./sixth_page/Circle.svg" alt="" />
+              <img src="./sixth_page/Circle (1).svg" alt="" />
+              <img src="./sixth_page/Circle (2).svg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="seventh_page">
+        <div className="content">
+          <div className="blue"></div>
+
+          <h2>Read our latest blogs & news</h2>
+
+          <div className="lasts">
+            <div className="left">
+              <img src="./seventh_page_assests/Image (2).png" alt="" />
+
+              <div className="firsts_box">
+                <div className="padding">
+                  <div className="small_p">Jan 19, 2021</div>
+                  <div className="h4_className">
+                    Today’s best design trends for digital products
+                  </div>
+                  <div className="link_tag">
+                    <a href="#">
+                      Read More <img src="./arrow.svg" alt="" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="right">
+              <img src="./seventh_page_assests/Image (3).png" alt="" />
+
+              <div className="firsts_box">
+                <div className="padding">
+                  <div className="small_p">Jan 19, 2021</div>
+                  <div className="h4_className">
+                    A practical guide to building a brand strategy
+                  </div>
+                  <div className="link_tag">
+                    <a href="#">
+                      Read More <img src="./arrow.svg" alt="" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="eighth_page">
+        <div className="container">
+          <div className="blue_box">
+            <img
+              className="logo_corner"
+              src="./eighth/left_corner.svg"
+              alt=""
+            />
+
+            <div className="content">
+              <div className="texts">
+                <div className="smallone">NEWSLETTER</div>
+                <div className="bigone">
+                  Subscribe our News Letter <br />
+                  to get Latest Updates.
+                </div>
+              </div>
+              <div className="input">
+                <form>
+                  <input
+                    type="text"
+                    name="search"
+                    placeholder="Paresh@Pixeto.com"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <img src="./sixth_page/siede_clmn.svg" alt="" />
+        </div>
+
+        <div className="contain2">
+          <div>
+            <img src="./eighth/corner.svg" alt="" />
+          </div>
+          <div className="total">
+            <div className="lft">
+              <div className="first">
+                Let's make <br />
+                something special
+              </div>
+
+              <div className="scnd">Let's talk! 🤙</div>
+              <div className="thrd">020 7993 2905</div>
+              <div className="four">hi@finsweet.com</div>
+              <div className="fift">
+                <div>
+                  DLF Cybercity, Bhubaneswar, <br />
+                  India, &52050
+                </div>
+              </div>
+            </div>
+
+            <div className="rght">
+              <div className="col1">
+                <a href="">Home</a>
+                <a href="">Service</a>
+                <a href="">Company</a>
+                <a href="">Career</a>
+                <a href="">News</a>
+              </div>
+
+              <div className="col2">
+                <a className="head_link" href="#">
+                  Service
+                </a>
+
+                <a href="">Technical support</a>
+                <a href="">Testing</a>
+                <a href="">Development</a>
+                <a href="">AWS/Azure </a>
+                <a href="">Consulting</a>
+                <a href="">Information Technology</a>
+              </div>
+
+              <div className="col3">
+                <a className="head_link" href="#">
+                  Resources
+                </a>
+
+                <a href="">About Us</a>
+                <a href="">Testimonial</a>
+                <a href="">Privacy Police</a>
+                <a href="">Terms of Use</a>
+                <a href="">Blog</a>
+
+                <div className="contact">
+                  <a href="#">
+                    <img src="./eighth/last_icon.svg" alt="" /> Contact Us
+                    <img src="./arrow.svg" alt="" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <footer>
+          <div className="content">
+            <div className="header">
+              <img src="./eighth/Logo (1).svg" alt="finsweet" />
+
+              <p>©2021 Finsweet</p>
+            </div>
+
+            <nav>
+              <p>
+                <a href="#" className="className_header">
+                  <img src="./eighth/Facebook/Negative.svg" alt="" />
+                </a>
+              </p>
+              <p>
+                <a href="#" className="className_header">
+                  <img src="./eighth/Twitter/Negative.svg" alt="" />
+                </a>
+              </p>
+
+              <p>
+                <a href="#" className="className_header">
+                  <img src="./eighth/Instagram/Negative.svg" alt="" />
+                </a>
+              </p>
+              <p>
+                <a href="#" className="className_header">
+                  <img src="./eighth/LinkedIn/Negative.svg" alt="" />
+                </a>
+              </p>
+            </nav>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+};
